b3bea84002794339c891b1d71b52e9a9f98eea32
JJ Coldiron <jj.coldiron@outlook.com>
2024-01-17 20:03:11 -0800

    Added hero.js and basic movement

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./sprites/hero.png")
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-
+	ctx.imageSmoothingEnabled = false;
 	gameEngine.init(ctx);
 
+	gameEngine.addEntity(new hero(gameEngine));
 	gameEngine.start();
 });

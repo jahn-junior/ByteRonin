911f662e187d381e319dcce267f8075817957588
Halim Lee <leehalim10@gmail.com>
2024-01-20 02:52:09 -0800

    flushed out all the code, background is now animated, and code based as been roughly cleaned

--- arena.js
+++ arena.js
@@ -27,6 +27,22 @@ class Floor1 {
     };
 
     draw(ctx) {
-        ctx.drawImage(this.spritesheet, 0, 0, 129, 65, this.x- this.game.camera.x, this.y,  PARAMS.BLOCKWIDTH * 2, PARAMS.BLOCKWIDTH);
+        ctx.drawImage(this.spritesheet, 0, 0, 129, 65, this.x- this.game.camera.x, this.y,  PARAMS.BLOCKWIDTH * 5, PARAMS.BLOCKWIDTH * 2.5);
+    };
+}
+class Pillar1 {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/ground.png");
+    }
+    load() {
+
+    };
+    update() {
+
+    };
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 129, 0, 255, 65, this.x- this.game.camera.x, this.y,  PARAMS.BLOCKWIDTH * 5, PARAMS.BLOCKWIDTH * 2.5);
     };
 }
